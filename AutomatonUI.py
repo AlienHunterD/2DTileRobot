f5b0c3608b0faf1284c4701e1b012f63ad3ac239
AlienHunterD <dbiediger@gmail.com>
2020-02-17 14:46:31 -0600

    Updated

--- AutomatonUI.py
+++ AutomatonUI.py
@@ -88,7 +88,7 @@ class AutomatonUIApp:
         for name,value in zip(["Initial Search", "Add/Shift Tile", "Delete Tile", "Move/Search"], self.board.results[4]):
             print("{} - {}".format(name, value))
         
-        print("Total Moves: {}".format(sum(self.board.results[4])))
+        #print("Total Moves: {}".format(sum(self.board.results[4])))
         
     
     def Iterate(self):
